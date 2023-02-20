2795e7e2a26c7106c8799900ab0dc8eef028a1a7
Foluke1 <koladefoluke84@gmail.com>
2023-02-20 15:39:46 -0800

    correct

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,23 @@
-#include <stdio.h>
+
+
+/*
+ * File: 101-quote.c
+ * Auth: Brennan D Baraban
+ */
+
 #include <unistd.h>
+
 /**
- * main-Entry point
- * void: When value X is void, it means it is not acceptions any arg
- (*Description -Prints out the last part of a quote in the standard error.)?
-*
- (unistd header:has the function write)
-*Return: 1 only is success.
-*/
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *                followed by a new line, to standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
 	write(2,
-		"and that piece of art is useful\"-Dora Korpar, 2015-10-19\n",59);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
